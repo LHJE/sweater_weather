3c880ef865a1e0d8014a7469b211721ec9c2ec9e
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-06 20:24:45 -0700

    Update weather database spec with tests for the json

--- spec/services/weather_database_spec.rb
+++ spec/services/weather_database_spec.rb
@@ -6,5 +6,21 @@ RSpec.describe "Weather Database" do
     lon = -104.984853
 
     weather = WeatherDatabase.get_weather(lat, lon)
+
+    expect(weather).to be_a(Hash)
+    expect(weather[:current]).to be_a(Hash)
+    expect(weather[:daily]).to be_an(Array)
+    expect(weather[:current][:humidity]).to be_a(Numeric)
+    expect(weather[:current][:temp]).to be_a(Numeric)
+    expect(weather[:current][:weather]).to be_an(Array)
+    expect(weather[:current][:weather][0]).to be_a(Hash)
+    expect(weather[:current][:weather][0][:description]).to be_a(String)
+
+    expect(weather[:daily][0]).to be_a(Hash)
+    expect(weather[:daily][0][:humidity]).to be_a(Numeric)
+    expect(weather[:daily][0][:temp][:day]).to be_a(Numeric)
+    expect(weather[:daily][0][:weather]).to be_an(Array)
+    expect(weather[:daily][0][:weather][0]).to be_a(Hash)
+    expect(weather[:daily][0][:weather][0][:description]).to be_a(String)
   end
 end
