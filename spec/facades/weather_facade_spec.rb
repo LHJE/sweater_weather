86ca587017d7aa74ede05525cbfb88329c70d6ba
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-06 20:48:19 -0700

    Update weather facade spec with more tests

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -6,5 +6,20 @@ RSpec.describe 'Weather Facade' do
 
     forecast = WeatherFacade.get_forecast(lat, lon)
 
+    expect(forecast).to be_a(Hash)
+    expect(forecast[:current]).to be_a(Hash)
+    expect(forecast[:daily]).to be_an(Array)
+    expect(forecast[:current][:humidity]).to be_a(Numeric)
+    expect(forecast[:current][:temp]).to be_a(Numeric)
+    expect(forecast[:current][:weather]).to be_an(Array)
+    expect(forecast[:current][:weather][0]).to be_a(Hash)
+    expect(forecast[:current][:weather][0][:description]).to be_a(String)
+
+    expect(forecast[:daily][0]).to be_a(Hash)
+    expect(forecast[:daily][0][:humidity]).to be_a(Numeric)
+    expect(forecast[:daily][0][:temp][:day]).to be_a(Numeric)
+    expect(forecast[:daily][0][:weather]).to be_an(Array)
+    expect(forecast[:daily][0][:weather][0]).to be_a(Hash)
+    expect(forecast[:daily][0][:weather][0][:description]).to be_a(String)
   end
 end
