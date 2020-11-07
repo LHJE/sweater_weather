903ce2a67edc3ba4f2cab40cb67f6a3d098ae889
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-06 19:06:15 -0700

    Update weather controller show method to have render json: ...  LocationSerializer

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::WeatherController < ApplicationController
 
   def show
     lat_and_long = SearchFacade.find_lat_and_long(params[:location])
-
+    render json: LocationSerializer.new(@weather)
 
   end
 
