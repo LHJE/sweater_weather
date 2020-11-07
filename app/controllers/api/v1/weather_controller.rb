24194b188d146bec9330ee2cc9f76485e344960a
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-06 20:48:38 -0700

    Update weather controller

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::WeatherController < ApplicationController
 
   def show
     lat_and_long = SearchFacade.find_lat_and_long(params[:location])
-    @weather = WeatherFacade.get_forecast(lat_and_long[:lat], lat_and_long[:lng])
+    weather = WeatherFacade.get_forecast(lat_and_long[:lat], lat_and_long[:lng])
   end
 
 end
