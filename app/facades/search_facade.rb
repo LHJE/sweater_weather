10cbabe6f1a9749cf0d553160e46c7a8362e0bd8
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-06 19:03:38 -0700

    Update search facade find lat long method

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,6 +1,5 @@
 class SearchFacade
   def self.find_lat_and_long(location)
-    attributes = LocationDatabase.get_lat_and_long(location)
-
+    LocationDatabase.get_lat_and_long(location)[:results][0][:locations][0][:latLng]
   end
 end
