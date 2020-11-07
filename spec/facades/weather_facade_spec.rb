0da6dd800d00bb2933f7a4f8deeea08e1268ef43
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-06 20:17:11 -0700

    Update weather facade spec

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 RSpec.describe 'Weather Facade' do
   it "It returns forecast poro" do
-    lat = 39.74
-    lon = -104.98
-    require "pry"; binding.pry
+    lat = 39.738453
+    lon = -104.984853
+
     forecast = WeatherFacade.get_forecast(lat, lon)
 
   end
