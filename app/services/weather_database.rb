cb5156298f0da98fbd7cae03298bb93a909ff985
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-06 20:47:50 -0700

    Update weather database

--- app/services/weather_database.rb
+++ app/services/weather_database.rb
@@ -1,6 +1,6 @@
 class WeatherDatabase
   def self.get_weather(lat, lon)
-    post_parsed_json("/data/2.5/onecall", lat, lon)
+    post_parsed_json("data/2.5/onecall", lat, lon)
   end
 
   def self.post_parsed_json(url, lat, lon)
