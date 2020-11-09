839c4f5cdac98faa59d827a64309a1f8163906ea
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 11:03:33 -0700

    Add forecast to trails facade

--- app/facades/trails_facade.rb
+++ app/facades/trails_facade.rb
@@ -1,6 +1,7 @@
 class TrailsFacade
   def self.get_trails(lat, lon)
     trails = TrailsDatabase.get_trails(lat, lon)
-    Trails.new(trails, lat, lon)
+    forecast = WeatherFacade.get_forecast(lat, lon).current_weather
+    Trails.new(trails, forecast, lat, lon)
   end
 end
