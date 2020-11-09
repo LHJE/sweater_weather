696790c257dc1268616a96b9ea3e95227f738677
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 10:30:49 -0700

    update one trail to use lat/lon to get distance to trail

--- app/poros/one_trail.rb
+++ app/poros/one_trail.rb
@@ -5,14 +5,18 @@ class OneTrail
               :difficulty,
               :location
 
-  def initialize(data)
+  def initialize(data, lat, lon)
     @id = nil
     require "pry"; binding.pry
     @name = data[:name]
     @summit = data[:summary]
     @difficulty = data[:difficulty]
     @location = data[:location]
+    @distance_to_trail = get_distance([lat, lon], [data[:latitude], data[:longitude]])
 
+    def get_distance(starting_location, ending_location)
+      SearchFacade.find_distance(starting_location, ending_location)
+    end
   end
 
 end
