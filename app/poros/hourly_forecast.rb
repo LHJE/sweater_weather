0b278a49c3fa09a1c4276bcacea9566b803dd28e
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-08 20:21:20 -0700

    Update hourly_forecast poro to focus more on what the US is asking for

--- app/poros/hourly_forecast.rb
+++ app/poros/hourly_forecast.rb
@@ -5,12 +5,20 @@ class HourlyForecast
               :icon
 
   def initialize(data)
-    @hour = formatted_hour(data[:dt])
+    @id = nil
+    @hour = formatted_time(data[:dt])
     @temperature = data[:temp]
+    @wind_speed = data[:wind_speed]
+    @wind_direction = readable_compass(data[:wind_deg])
+    @conditions = data[:weather][0][:description]
     @icon = data[:weather][0][:icon]
   end
 
-  def formatted_hour(iso)
-    Time.at(iso)
+  def formatted_time(iso)
+    Time.at(iso).to_s.at(11..18)
+  end
+
+  def readable_compass(degrees)
+    CompassPoint.compass_quadrant_bearing(degrees)
   end
 end
