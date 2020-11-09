599a9031ace293c372107d397e0fd91378961229
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 16:25:05 -0700

    Create Image controller with show method

---
+++ app/controllers/api/v1/image_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::ImageController < ApplicationController
+
+  def show
+    lat_and_long = SearchFacade.find_lat_and_long(params[:location])
+    forecast = WeatherFacade.get_current_forecast(lat_and_long[:lat], lat_and_long[:lng])
+    image = ImageFacade.get_image(forecast[:weather][0][:main], params[:location])
+    render json: ImageSerializer.new(image)
+  end
+
+end
