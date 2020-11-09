901048dd9afb2b01690096fe8efc30a010b0efd6
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 11:26:25 -0700

    Update weather database spec to test get_current_weather

--- spec/services/weather_database_spec.rb
+++ spec/services/weather_database_spec.rb
@@ -46,4 +46,32 @@ RSpec.describe "Weather Database" do
 
     expect(weather[:minutely]).to be_a(NilClass)
   end
+
+  it "It returns trail forecast" do
+    lat = 39.738453
+    lon = -104.984853
+
+    weather = WeatherDatabase.get_current_weather(lat, lon)
+
+    expect(weather).to be_a(Hash)
+    expect(weather[:dt]).to be_a(Numeric)
+    expect(weather[:sunrise]).to be_a(Numeric)
+    expect(weather[:sunset]).to be_a(Numeric)
+    expect(weather[:temp]).to be_a(Numeric)
+    expect(weather[:feels_like]).to be_a(Numeric)
+    expect(weather[:pressure]).to be_a(Numeric)
+    expect(weather[:humidity]).to be_a(Numeric)
+    expect(weather[:dew_point]).to be_a(Numeric)
+    expect(weather[:uvi]).to be_a(Numeric)
+    expect(weather[:clouds]).to be_a(Numeric)
+    expect(weather[:visibility]).to be_a(Numeric)
+    expect(weather[:wind_speed]).to be_a(Numeric)
+    expect(weather[:wind_deg]).to be_a(Numeric)
+    expect(weather[:weather]).to be_a(Array)
+    expect(weather[:weather][0]).to be_a(Hash)
+    expect(weather[:weather][0][:id]).to be_a(Numeric)
+    expect(weather[:weather][0][:main]).to be_a(String)
+    expect(weather[:weather][0][:description]).to be_a(String)
+    expect(weather[:weather][0][:icon]).to be_a(String)
+  end
 end
