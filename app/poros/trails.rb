6f86dc4d2f75ab3d315e4cde4c1cdebacb367426
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 09:46:00 -0700

    Update trails poro to use OneTrail poro

--- app/poros/trails.rb
+++ app/poros/trails.rb
@@ -1,8 +1,16 @@
 class Trails
-  attr_reader :id
+  attr_reader :id,
+              :trails
 
   def initialize(data)
     @id = nil
+    @trails = format_trails(data)
+  end
+
+  def format_trails(data)
+    data.map do |day|
+      OneTrail.new(day)
+    end
   end
 
 end
