d7aa73cd1f3c22663d1848fedf06dfcdab4a8e5b
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 10:02:48 -0700

    Move distance test to location database

--- spec/services/location_database_spec.rb
+++ spec/services/location_database_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe "Location Database" do
     expect(location[:options]).to be_an(NilClass)
     expect(location[:results]).to be_an(NilClass)
   end
+
+  it "Can return distance for two geolocations" do
+    place_1 = [39.738453, -104.984853]
+    place_2 = [39.9388, -105.2582]
+
+    distance = LocationDatabase.get_trails(place_1, place_2)
+
+    expect(distance).to be_a(Hash)
+  end
 end
