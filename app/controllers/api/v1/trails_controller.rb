2be4646c2d0cffb06d80066f24d97cc932b573ae
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 11:04:48 -0700

    Take forecast out of trails controller and add json render method

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::TrailsController < ApplicationController
 
   def show
     lat_and_long = SearchFacade.find_lat_and_long(params[:location])
-    forecast = WeatherFacade.get_forecast(lat_and_long[:lat], lat_and_long[:lng])
     trails = TrailsFacade.get_trails(lat_and_long[:lat], lat_and_long[:lng])
+    render json: TrailsSerializer.new(trails)
   end
 
 end
