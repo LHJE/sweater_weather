ce5e17ce0cbdf666af571de7edd1402d4abcd247
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 11:27:09 -0700

    Rename get_trail_weather to get_current_weather to be more precise

--- app/services/weather_database.rb
+++ app/services/weather_database.rb
@@ -15,11 +15,11 @@ class WeatherDatabase
     JSON.parse(response.body, symbolize_names: :true)
   end
 
-  def self.get_trail_weather(lat, lon)
-    post_parsed_trail_json("data/2.5/onecall", lat, lon)
+  def self.get_current_weather(lat, lon)
+    post_parsed_current_json("data/2.5/onecall", lat, lon)
   end
 
-  def self.post_parsed_trail_json(url, lat, lon)
+  def self.post_parsed_current_json(url, lat, lon)
     response = Faraday.get("#{ENV['WEATHER_URL']}/#{url}") do |f|
       f.params['appid'] = ENV['WEATHER_API_KEY']
       f.params['lat'] = lat
