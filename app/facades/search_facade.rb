f4f6be35f6ca259c2101c4400fcdfb5c7a4cc069
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 11:04:14 -0700

    Correct typo in search facade

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -5,8 +5,7 @@ class SearchFacade
 
   def self.find_distance(starting_location, ending_location)
     starting_hash = LocationDatabase.find_closest_address(starting_location)
-    starting_address = "#{starting_hash[:street]} #{starting_hash[:adminArea5]} #{starting_hash[:adminArea3]} #{starting_hash[:postal_code
-    ]}"
+    starting_address = "#{starting_hash[:street]} #{starting_hash[:adminArea5]} #{starting_hash[:adminArea3]} #{starting_hash[:postal_code]}"
     ending_hash = LocationDatabase.find_closest_address(ending_location)
     ending_address = "#{ending_hash[:street]} #{ending_hash[:adminArea5]} #{ending_hash[:adminArea3]} #{ending_hash[:postal_code
     ]}"
