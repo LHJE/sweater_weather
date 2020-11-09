f3627a3816ad60cc2a9c987d3205dd6b6bb875c3
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 09:24:32 -0700

    Add trails database spec

---
+++ spec/services/trails_database_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Trails Database" do
+  it "Can return trails data for a geolocation" do
+    lat = 39.738453
+    lon = -104.984853
+
+    trails = TrailsDatabase.get_trails(lat, lon)
+
+    expect(trails).to be_a(Hash)
+  end
+end
