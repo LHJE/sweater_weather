d373abd6ccbac3b96150b27e1ebf4e2ea926d535
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 11:36:36 -0700

    Update trails serializer spec to be more robust

--- spec/requests/api/v1/trails_serializer_spec.rb
+++ spec/requests/api/v1/trails_serializer_spec.rb
@@ -6,7 +6,24 @@ RSpec.describe TrailsSerializer do
     expect(response).to be_successful
 
     rsp = JSON.parse(response.body, symbolize_names: :true)
-
+    
     expect(rsp).to be_a(Hash)
+    expect(rsp[:data]).to be_a(Hash)
+    expect(rsp[:data][:id]).to be_a(NilClass)
+    expect(rsp[:data][:type]).to be_a(String)
+
+    expect(rsp[:data][:attributes]).to be_a(Hash)
+    expect(rsp[:data][:attributes][:id]).to be_a(NilClass)
+    expect(rsp[:data][:attributes][:forecast]).to be_a(Hash)
+    expect(rsp[:data][:attributes][:forecast][:summary]).to be_a(String)
+    expect(rsp[:data][:attributes][:forecast][:temperature]).to be_a(Numeric)
+    expect(rsp[:data][:attributes][:forecast][:trails]).to be_a(Array)
+
+    expect(rsp[:data][:attributes][:trails][0]).to be_a(Hash)
+    expect(rsp[:data][:attributes][:trails][0][:id]).to be_a(NilClass)
+    expect(rsp[:data][:attributes][:trails][0][:name]).to be_a(String)
+    expect(rsp[:data][:attributes][:trails][0][:summary]).to be_a(String)
+    expect(rsp[:data][:attributes][:trails][0][:location]).to be_a(String)
+    expect(rsp[:data][:attributes][:trails][0][:distance_to_trail]).to be_a(String)
   end
 end
