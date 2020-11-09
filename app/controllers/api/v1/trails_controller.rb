4e4915443c4a467b3556ceaa72711929942fd762
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 09:18:21 -0700

    Add trails controller

---
+++ app/controllers/api/v1/trails_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::TrailsController < ApplicationController
+
+  def show
+    lat_and_long = SearchFacade.find_lat_and_long(params[:location])
+    forecast = WeatherFacade.get_forecast(lat_and_long[:lat], lat_and_long[:lng])
+    
+  end
+
+end
