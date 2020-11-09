a649461ae404f111dc47196eaa0aa96ee60b9db3
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 09:31:46 -0700

    Add trails database

---
+++ app/services/trails_database.rb
@@ -0,0 +1,17 @@
+class TrailsDatabase
+  def self.get_trails(lat, long)
+    key = ENV['TRAILS_API_KEY']
+    url = ENV['TRAILS_URL']
+    uri = "get-trails?lat=#{lat}&lon=#{long}&maxDistance=30&key=#{key}"
+    get_results(url, key, uri)
+  end
+
+  def self.get_results(url, key, uri)
+    conn = Faraday.new(url: url) do |faraday|
+      faraday.headers['X-API-Key'] = key
+    end
+    results = conn.get(uri)
+    require "pry"; binding.pry
+    JSON.parse(results.body, symbolize_names: true)
+  end
+end
