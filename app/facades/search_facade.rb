e1439e01ca24becaf1327fe4575ef522a2600937
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 09:57:53 -0700

    update search facade with find distance method

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -2,4 +2,9 @@ class SearchFacade
   def self.find_lat_and_long(location)
     LocationDatabase.get_lat_and_long(location)
   end
+
+  def self.find_distance(starting_location, ending_location)
+    attributes = DistanceDatabase.find_distance(starting_location, ending_location)
+
+  end
 end
