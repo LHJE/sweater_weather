5f4b89c29aae8e5e0981025b9646deb4acd3b3a0
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 09:24:14 -0700

    Update trails controller to search for trails

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::TrailsController < ApplicationController
   def show
     lat_and_long = SearchFacade.find_lat_and_long(params[:location])
     forecast = WeatherFacade.get_forecast(lat_and_long[:lat], lat_and_long[:lng])
-    
+    trails = TrailsFacade.get_trails(lat_and_long[:lat], lat_and_long[:lng])
   end
 
 end
