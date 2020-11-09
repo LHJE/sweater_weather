1b5ea7e60198705fd0c6fa4b66ee099c24785cc0
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 11:04:29 -0700

    Correct typo/method placement in one trail poro

--- app/poros/one_trail.rb
+++ app/poros/one_trail.rb
@@ -7,16 +7,15 @@ class OneTrail
 
   def initialize(data, lat, lon)
     @id = nil
-    require "pry"; binding.pry
     @name = data[:name]
     @summit = data[:summary]
     @difficulty = data[:difficulty]
     @location = data[:location]
     @distance_to_trail = get_distance([lat, lon], [data[:latitude], data[:longitude]])
+  end
 
-    def get_distance(starting_location, ending_location)
-      SearchFacade.find_distance(starting_location, ending_location)
-    end
+  def get_distance(starting_location, ending_location)
+    SearchFacade.find_distance(starting_location, ending_location)
   end
 
 end
