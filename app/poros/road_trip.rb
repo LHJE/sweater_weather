947f5a529220547ac523c9661ab92e5839debc5e
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-10 13:13:02 -0700

    update road trip poro forcast with if there is no route

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -14,20 +14,28 @@ class RoadTrip
   end
 
   def get_route_time(time)
-    distance_of_time_in_words(time)
+    if time.nil?
+      "This destination is not reachable"
+    else
+      distance_of_time_in_words(time)
+    end
   end
 
   def get_future_weather_forecast(time, forecasts)
-    future_time = Time.new + time
-    if future_time.min < 31
-      future_forecast = forecasts.find do |forecast|
-        forecast.hour.to_time.hour == future_time.hour
-      end
+    if time.nil?
+      {temperature: forecasts[0].temperature, conditions: forecasts[0].conditions}
     else
-      future_forecast = forecasts.find do |forecast|
-        forecast.hour.to_time.hour == future_time.hour + 1
+      future_time = Time.new + time
+      if future_time.min < 31
+        future_forecast = forecasts.find do |forecast|
+          forecast.hour.to_time.hour == future_time.hour
+        end
+      else
+        future_forecast = forecasts.find do |forecast|
+          forecast.hour.to_time.hour == future_time.hour + 1
+        end
       end
+
     end
-    {temperature: future_forecast.temperature, conditions: future_forecast.conditions}
   end
 end
