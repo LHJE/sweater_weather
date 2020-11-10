916050b18c94e53ddd2daf35f76bfbe403e6fb6a
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-10 12:45:20 -0700

    Update weather database to search for just hourly weather

--- app/services/weather_database.rb
+++ app/services/weather_database.rb
@@ -15,6 +15,22 @@ class WeatherDatabase
     JSON.parse(response.body, symbolize_names: :true)
   end
 
+  def self.get_hourly_weather(lat, lon)
+    post_parsed_hourly_json("data/2.5/onecall", lat, lon)
+  end
+
+  def self.post_parsed_hourly_json(url, lat, lon)
+    response = Faraday.get("#{ENV['WEATHER_URL']}/#{url}") do |f|
+      f.params['appid'] = ENV['WEATHER_API_KEY']
+      f.params['lat'] = lat
+      f.params['lon'] = lon
+      f.params['exclude'] = 'minutely,daily,current'
+      f.params['units'] = 'imperial'
+    end
+
+    JSON.parse(response.body, symbolize_names: :true)
+  end
+
   def self.get_current_weather(lat, lon)
     post_parsed_current_json("data/2.5/onecall", lat, lon)
   end
