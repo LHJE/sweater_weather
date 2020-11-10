548e6981f39c31f3044a82d03a18b97370cf24ee
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 20:29:40 -0700

    Update user serializer spec to tests for a matching email

--- spec/requests/api/v1/user_serializer_spec.rb
+++ spec/requests/api/v1/user_serializer_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe UserSerializer do
   before :each do
     @user = User.create(email: 'rt@a.com', password: 'a', password_confirmation: 'a', api_key: "9EjiyN2w8u1fETO20rWVDA==")
   end
-  it "Sends user information" do
+
+  it "Sends user information if the user is able to be created" do
     json = {
             "email": "whatever@example.com",
             "password": "password",
@@ -16,7 +17,29 @@ RSpec.describe UserSerializer do
 
     rsp = JSON.parse(response.body, symbolize_names: :true)
 
-require "pry"; binding.pry
+    expect(rsp).to be_a(Hash)
+    expect(rsp[:data]).to be_a(Hash)
+    expect(rsp[:data][:id]).to be_a(String)
+    expect(rsp[:data][:type]).to eq("user")
+
+    expect(rsp[:data][:attributes]).to be_a(Hash)
+    expect(rsp[:data][:attributes][:email]).to be_a(String)
+    expect(rsp[:data][:attributes][:api_key]).to be_a(String)
+  end
+
+  it "Sends does not user information if the user has a matching email address" do
+    json = {
+            "email": "rt@a.com",
+            "password": "password",
+            "password_confirmation": "password"
+          }
+
+    post '/api/v1/users', params: json.to_json
+    require "pry"; binding.pry
+    expect(response).to be_unsuccessful
+
+    rsp = JSON.parse(response.body, symbolize_names: :true)
+
     expect(rsp).to be_a(Hash)
     expect(rsp[:data]).to be_a(Hash)
     expect(rsp[:data][:id]).to be_a(String)
