eb3d743a25a9e7880e6b5e2c1ad4817df034259f
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 17:55:52 -0700

    Add user serializer spec

---
+++ spec/requests/api/v1/user_serializer_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe UserSerializer do
+  it "Sends user information" do
+    json = {
+            "email": "whatever@example.com",
+            "password": "password",
+            "password_confirmation": "password"
+          }
+
+    post '/api/v1/users', params: json
+    expect(response).to be_successful
+
+    rsp = JSON.parse(response.body, symbolize_names: :true)
+
+require "pry"; binding.pry
+
+  end
+end
