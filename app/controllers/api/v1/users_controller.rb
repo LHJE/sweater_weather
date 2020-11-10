3e076c93ed5485f544b5f47791c816d25a94e9ce
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 20:18:00 -0700

    Update user controller to create user model and save a unique api key

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-
-    user_data = JSON.parse(request.raw_post)
-    user = User.new(user_data)
-    render json: UserSerializer.new(user)
+    user_params = JSON.parse(request.raw_post, symbolize_names: true)
+    user = User.create!(user_params)
+    user.api_key = SecureRandom.base64(16)
+    user.save
+    require "pry"; binding.pry
+    render json: UserSerializer.new(user), status: 201
   end
-
 end
