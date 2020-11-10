ead8a9930226da1a4489c50199771fdc405c0d2f
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-10 11:00:27 -0700

    Update road trip controller for if API Key doesn't exist in system

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::RoadTripController < ApplicationController
 
   def create
-
+    trip_params = JSON.parse(request.raw_post, symbolize_names: true)
+    user = User.find_by(api_key: trip_params[:api_key])
+    if user.nil?
+      render json: {error: "API Key Not In Our System"} , status: 401
+    else
+      require "pry"; binding.pry
+      
+      render json: RoadTripSerializer.new(user), status: 201
+    end
   end
 
 
