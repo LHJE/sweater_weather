8f61b30d0e47083b84e8f66e18e3427e2aa6f121
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 20:29:55 -0700

    Add conditional to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,10 +2,14 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user_params = JSON.parse(request.raw_post, symbolize_names: true)
-    user = User.create!(user_params)
+    user = User.new(user_params)
     user.api_key = SecureRandom.base64(16)
-    user.save
     require "pry"; binding.pry
-    render json: UserSerializer.new(user), status: 201
+    if user.save
+      user.save
+      render json: UserSerializer.new(user), status: 201
+    else
+      render json: user.errors.full_messages.to_sentence, status: 400
+    end
   end
 end
