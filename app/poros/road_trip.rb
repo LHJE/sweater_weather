0359885dcf3bdbad5871707bc42ae76334ceb75f
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-10 14:09:40 -0700

    Update road trip to calculate the correct forecast instead of just ... the first forecast that matches the hour

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -7,10 +7,20 @@ class RoadTrip
               :weather_at_eta
 
   def initialize(trip_params, forecast, route)
+    time = get_time(route[:formattedTime])
     @start_city = trip_params[:origin]
     @end_city = trip_params[:destination]
-    @travel_time = get_route_time(route[:realTime])
-    @weather_at_eta = get_future_weather_forecast(route[:realTime], forecast)
+    @travel_time = get_route_time(time)
+    @weather_at_eta = get_future_weather_forecast(time, forecast)
+  end
+
+  def get_time(time)
+    if time.nil?
+      nil
+    else
+      split_time = time.split(":").map(&:to_i)
+      (((split_time[0] * 60) + split_time[1]) * 60) + split_time[2]
+    end
   end
 
   def get_route_time(time)
@@ -25,17 +35,28 @@ class RoadTrip
     if time.nil?
       {temperature: forecasts[0].temperature, conditions: forecasts[0].conditions}
     else
-      future_time = Time.new + time
-      if future_time.min < 31
-        future_forecast = forecasts.find do |forecast|
-          forecast.hour.to_time.hour == future_time.hour
-        end
+      if time > 172800
+        "There is no forcast that far into the future"
       else
-        future_forecast = forecasts.find do |forecast|
-          forecast.hour.to_time.hour == future_time.hour + 1
+        future_time = Time.new + time
+        date = future_time.to_s.split(" ")[0]
+        if future_time.min < 31
+          future_forecast = forecasts.find do |forecast|
+            if forecast.hour.split(" ")[0] == date
+              forecast.hour.to_time.hour == future_time.hour
+            end
+          end
+        else
+          future_forecast = forecasts.find do |forecast|
+            if forecast.hour.split(" ")[0] == date
+              forecast.hour.to_time.hour == future_time.hour + 1
+            else
+              forecast.hour.to_time.hour == future_time.hour
+            end
+          end
         end
+        {temperature: future_forecast.temperature, conditions: future_forecast.conditions}
       end
-
     end
   end
 end
