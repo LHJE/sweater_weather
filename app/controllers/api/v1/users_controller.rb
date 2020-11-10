4129f980e2e8a36acf9f53b92ecb4e058dc65cb6
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 20:34:27 -0700

    Update users controller to render to json a hash instead of an object

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,12 +4,12 @@ class Api::V1::UsersController < ApplicationController
     user_params = JSON.parse(request.raw_post, symbolize_names: true)
     user = User.new(user_params)
     user.api_key = SecureRandom.base64(16)
-    require "pry"; binding.pry
+    # require "pry"; binding.pry
     if user.save
       user.save
       render json: UserSerializer.new(user), status: 201
     else
-      render json: user.errors.full_messages.to_sentence, status: 400
+      render json: {error: user.errors.full_messages.to_sentence} , status: 400
     end
   end
 end
