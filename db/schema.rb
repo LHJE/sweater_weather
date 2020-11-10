810451c5ff77b2d127c516a0518db19e83d3dd2e
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 19:56:07 -0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2020_11_10_023552) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
-    t.string "api"
+    t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
