7a9bb56057ae892ab9a691ccf2fc5fe45206e90f
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 21:16:27 -0700

    Add conditional for login on users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,7 +15,11 @@ class Api::V1::UsersController < ApplicationController
   def login
     user_params = JSON.parse(request.raw_post, symbolize_names: true)
     user = User.where(email: user_params[:email]).first
-    
-    require "pry"; binding.pry
+    if user == nil || user.authenticate(params[:password])
+      require "pry"; binding.pry
+      render json: {error: user.errors.full_messages.to_sentence} , status: 400
+    else
+      render json: UserSerializer.new(user), status: 200
+    end
   end
 end
