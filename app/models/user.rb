df949ab60037c90386719fa8f96ed2621a6b2e8a
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 20:18:11 -0700

    Update user model to have api_key validated

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :password, presence: { require: true }
   validates :password_confirmation, presence: { require: true }
+  validates :api_key, confirmation: true, unless: -> { api_key.blank? }
 
 
 end
