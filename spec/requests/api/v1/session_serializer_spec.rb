7e56a40c01c60293d5580a9f1981c9c748106a97
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 21:24:05 -0700

    Add sessions serializer spec test for no matching email

--- spec/requests/api/v1/session_serializer_spec.rb
+++ spec/requests/api/v1/session_serializer_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe SessionSerializer do
     @user = User.create(email: 'rt@a.com', password: 'a', password_confirmation: 'a', api_key: "9EjiyN2w8u1fETO20rWVDA==")
   end
 
-  it "Sends user information if the user is able to be created" do
+  it "Sends user information if the user is able to log in" do
     json = {
             "email": "rt@a.com",
             "password": "a",
@@ -24,6 +24,21 @@ RSpec.describe SessionSerializer do
     expect(rsp[:data][:attributes]).to be_a(Hash)
     expect(rsp[:data][:attributes][:email]).to be_a(String)
     expect(rsp[:data][:attributes][:api_key]).to be_a(String)
+  end
+
+  it "Sends error if the email is not in the system" do
+    json = {
+            "email": "rasdasdfasdfsdfaft@a.com",
+            "password": "a",
+          }
+
+    post '/api/v1/sessions', params: json.to_json
+    expect(response).to_not be_successful
+
+    rsp = JSON.parse(response.body, symbolize_names: :true)
+
+    expect(rsp).to be_a(Hash)
+    expect(rsp[:error]).to eq("Error: Email or Password Incorrect")
   end
 
 end
