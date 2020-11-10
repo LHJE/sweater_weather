e60610aeb6885955498cd715e26084a3309d1d42
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 21:13:48 -0700

    Update user controller with login method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,12 +4,18 @@ class Api::V1::UsersController < ApplicationController
     user_params = JSON.parse(request.raw_post, symbolize_names: true)
     user = User.new(user_params)
     user.api_key = SecureRandom.base64(16)
-    # require "pry"; binding.pry
     if user.save
       user.save
       render json: UserSerializer.new(user), status: 201
     else
       render json: {error: user.errors.full_messages.to_sentence} , status: 400
     end
+  end
+
+  def login
+    user_params = JSON.parse(request.raw_post, symbolize_names: true)
+    user = User.where(email: user_params[:email]).first
+    
+    require "pry"; binding.pry
   end
 end
