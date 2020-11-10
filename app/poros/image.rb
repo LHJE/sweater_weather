968d87699688b2dc012d304d4ba9d288c2ae15fd
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 19:22:58 -0700

    Refactor image poro to have dimensions to be a hash

--- app/poros/image.rb
+++ app/poros/image.rb
@@ -8,8 +8,9 @@ class Image
   def initialize(data, location)
     @location = location
     @image_url = data[:urls][:full]
-    @dimensions = []
-    @dimensions = [data[:width], data[:height]]
+    @dimensions = {}
+    @dimensions[:width] = data[:width]
+    @dimensions[:height] = data[:height]
     @credit = {}
     @credit[:author] = data[:user][:name]
     @credit[:author_profile] = data[:user][:links][:self]
