73ef2b7086a1386bf3fe79df85a2e276a5cebc92
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-09 19:24:53 -0700

    Refactor weather facade spec to remove id and icon expectation

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe 'Weather Facade' do
     expect(weather.current_weather[:conditions]).to be_a(String)
     expect(weather.current_weather[:feels_like]).to be_a(Numeric)
     expect(weather.current_weather[:humidity]).to be_a(Numeric)
-    expect(weather.current_weather[:icon]).to be_a(String)
     expect(weather.current_weather[:sunrise]).to be_a(String)
     expect(weather.current_weather[:sunset]).to be_a(String)
     # expect(weather.current_weather[:max_temp]).to be_a(Numeric)
@@ -25,7 +24,6 @@ RSpec.describe 'Weather Facade' do
     expect(weather.daily_weather[0]).to be_a(DailyForecast)
     expect(weather.daily_weather[0].date).to be_a(String)
     expect(weather.daily_weather[0].conditions).to be_a(String)
-    expect(weather.daily_weather[0].icon).to be_a(String)
     expect(weather.daily_weather[0].sunrise).to be_a(String)
     expect(weather.daily_weather[0].sunset).to be_a(String)
     expect(weather.daily_weather[0].max_temp).to be_a(Numeric)
@@ -35,7 +33,6 @@ RSpec.describe 'Weather Facade' do
 
     expect(weather.hourly_weather[0]).to be_a(HourlyForecast)
     expect(weather.hourly_weather[0].hour).to be_a(String)
-    expect(weather.hourly_weather[0].icon).to be_a(String)
     expect(weather.hourly_weather[0].temperature).to be_a(Numeric)
   end
 
