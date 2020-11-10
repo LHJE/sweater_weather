360832dd7a776563ea6931eb2608c6f1d08b3345
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-10 14:37:32 -0700

    Re add icon to hourly, and make wind read in mph

--- app/poros/hourly_forecast.rb
+++ app/poros/hourly_forecast.rb
@@ -3,14 +3,16 @@ class HourlyForecast
               :hour,
               :wind_speed,
               :wind_direction,
-              :conditions
+              :conditions,
+              :icon
 
   def initialize(data)
     @hour = formatted_time(data[:dt])
     @temperature = data[:temp]
-    @wind_speed = data[:wind_speed]
+    @wind_speed = "#{data[:wind_speed]} mph"
     @wind_direction = readable_compass(data[:wind_deg])
     @conditions = data[:weather][0][:description]
+    @icon = data[:weather][0][:icon]
   end
 
   def formatted_time(iso)
