c2b64ed7e4a51277130ebe3da51c4797d590f43c
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-10 17:45:53 -0700

    Further refactor road_trip poro

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -45,18 +45,26 @@ class RoadTrip
     if time > 172800
       "There is no forcast that far into the future"
     else
-      future_time = Time.new + time
-      date = future_time.to_s.split(" ")[0]
-      if future_time.min < 31
-        future_forecast = find_forecast(future_time.hour, forecasts, date)
-      else
-        future_forecast = find_forecast(future_time.hour + 1, forecasts, date)
-      end
-      {temperature: future_forecast.temperature, conditions: future_forecast.conditions}
+      find_future_forecast(time, forecasts)
     end
   end
 
-  def find_forecast(future_time_hour, forecasts, date)
+  def find_future_forecast(time, forecasts)
+    future_time = Time.new + time
+    date = future_time.to_s.split(" ")[0]
+    future_forecast = get_future_forecast_depending_on_time(future_time, forecasts, date)
+    {temperature: future_forecast.temperature, conditions: future_forecast.conditions}
+  end
+
+  def get_future_forecast_depending_on_time(future_time, forecasts, date)
+    if future_time.min < 31
+      find_specific_forecast(future_time.hour, forecasts, date)
+    else
+      find_specific_forecast(future_time.hour + 1, forecasts, date)
+    end
+  end
+
+  def find_specific_forecast(future_time_hour, forecasts, date)
     forecasts.find do |forecast|
       if forecast.hour.split(" ")[0] == date
         forecast.hour.to_time.hour == future_time_hour
